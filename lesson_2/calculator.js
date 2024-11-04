9889ce08e397c5a8408e5536f7288cfe82d91905
byoon97 <byoon5397@gmail.com>
2024-11-04 13:45:08 -0500

    additional calculator features

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,23 +1,49 @@
 const readline = require("readline-sync");
 
-console.log("Wecome to Calculator");
+const prompt = (message) => console.log(message);
+const invalidNumber = (num) =>
+  num.trimStart() === "" || Number.isNaN(Number(num));
 
-console.log("What is the first number");
-let number1 = readline.question();
+prompt("Wecome to Calculator");
 
-console.log("What is the second number?");
-let number2 = readline.question();
+while (true) {
+  prompt("What is the first number");
+  let number1 = readline.question();
 
-console.log("what is the operator?\n1) Add 2) Subtract 3) Multiply 4) Divide");
-let operator = readline.question();
+  while (invalidNumber(number1)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number1 = readline.question();
+  }
 
-let num1 = Number(number1);
-let num2 = Number(number2);
-let output;
+  prompt("What is the second number?");
+  let number2 = readline.question();
 
-if (operator === "1") output = num1 + num2;
-if (operator === "2") output = num1 - num2;
-if (operator === "3") output = num1 * num2;
-if (operator === "4") output = num1 / num2;
+  while (invalidNumber(number2)) {
+    prompt("Hmm... that doesn't look like a valid number.");
+    number2 = readline.question();
+  }
 
-console.log(`The result is ${output}`);
+  prompt("what is the operator?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+  let operator = readline.question();
+
+  while (!["1", "2", "3", "4"].includes(operator)) {
+    prompt("Must choose 1, 2, 3 or 4");
+    operator = readline.question();
+  }
+
+  let num1 = Number(number1);
+  let num2 = Number(number2);
+  let output;
+
+  if (operator === "1") output = num1 + num2;
+  if (operator === "2") output = num1 - num2;
+  if (operator === "3") output = num1 * num2;
+  if (operator === "4") output = num1 / num2;
+
+  prompt(`The result is ${output}`);
+
+  prompt("Would you like to perform another operation?\n 1) Yes 2) No)");
+  let answer = readline.question();
+
+  if (answer != "yes") break;
+}
